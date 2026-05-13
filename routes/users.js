e416a654859c7d9de1ee459301bbb8f6837b0161
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-12 23:23:08 -0300

    U02 - Implementação de acesso à API REST com Restify

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const assert = require('assert');
+const restify = require('restify-clients');
+const router = express.Router();
 
-/* GET users listing. */
+const client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  client.get('/users', (err, request, response, obj)=>{
+    assert.ifError(err);
+    res.end(JSON.stringify(obj,null, 2));
+  })
 });
 
 module.exports = router;
